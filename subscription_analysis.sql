f4a5853792a920d31b952814dd5a6244a640c0e0
Jacques Hervochon <62380585+jacquuouille@users.noreply.github.com>
2025-02-15 16:13:22 +0100

    Update subscription_analysis.sql

--- subscription_analysis.sql
+++ subscription_analysis.sql
@@ -1,1 +1,19 @@
--- test
+/* DATA IMPORTATION */
+
+CREATE TABLE IF NOT EXISTS public.streaming_data
+(
+    customer_id text COLLATE pg_catalog."default" NOT NULL,
+    created_date date NOT NULL,
+    canceled_date text COLLATE pg_catalog."default",
+    subscription_cost integer,
+    subscription_interval text COLLATE pg_catalog."default",
+    was_subscription_paid text COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.streaming_data
+    OWNER to postgres;
+
+
+-- 
