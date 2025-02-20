0f453f57277402593e963396b2e2e4133740ab7f
Jacques Hervochon <62380585+jacquuouille@users.noreply.github.com>
2025-02-20 12:31:40 +0100

    Update subscription_analysis.sql

--- subscription_analysis.sql
+++ subscription_analysis.sql
@@ -147,7 +147,7 @@ order by
 	1 desc
 
 
--- 1.2. Active Accounts Over Time  
+-- 1.2. Active Accounts Over Time (from the date subscribers joined for the first time, regardless of whether they were later recovered)
 -- using the temporary tables 'data_prep', 'new_users', 'reccuring_users' and 'recovered_users' created in the previous query
 
 (...)
@@ -171,3 +171,101 @@ group by
 	1 
 order by 
 	1 
+
+	
+-- 1.3. Subscriber Tenure Distribution (from the date subscribers stayed active for the last time (only the reactivation date of recovered accounts will be taken into account)
+-- using the temporary tables 'data_prep', 'fast_churn', 'churned_users', 'new_users', 'reccuring_users' and 'recovered_users' created in the previous query
+
+(...)
+, inactive_subscribers as ( 
+	select 'inactive' as main_category, category, customer_id from fast_churn
+	union 
+	select 'inactive' as main_category, category, customer_id from churned_users 
+)
+, active_subscribers as (
+	select 'active' as main_category, category, customer_id from new_users 
+	union 
+	select 'active' as main_category, category, customer_id from reccuring_users 
+	union 
+	select 'active' as main_category, category, customer_id from recovered_users
+)
+, all_together as (
+	select * from inactive_subscribers
+	union 
+	select * from active_subscribers
+) 
+, first_touch as (
+	select 
+		t1.customer_id
+		-- for recovered accounts, we need to take into account the date they subscribed again (tenure is from their last active subscription, and not from when they subscribes to the plan for the first time)
+		, case 
+			when t2.category = 'recovered' then t3.created_date else t1.created_date  
+		  end as first_touch_date
+		, t2.category
+		, t2.main_category
+	from 
+		data_prep t1
+	join 
+		all_together t2
+		on t1.customer_id = t2.customer_id
+	left join 
+		recovered_users t3
+		on t1.customer_id = t3.customer_id
+	where 
+		t1.first_activity_event = 1
+) 
+, last_touch as (
+	select 
+		t1.customer_id
+		, t1.canceled_date as last_touch_date 
+		, t2.category
+		, t2.main_category
+	from 
+		data_prep t1
+	join 
+		all_together t2
+		on t1.customer_id = t2.customer_id
+	where 
+		t1.last_activity_event = 1
+) 
+, subscribers_tenure as ( 
+	select 
+		t1.customer_id 
+		, t1.main_category
+		, t1.category
+		, t1.first_touch_date
+		, coalesce(t2.last_touch_date, '2023-09-08') as last_touch_date
+		, extract(month from age(coalesce(t2.last_touch_date, '2023-09-08'), t1.first_touch_date)) as month_tenure 
+	from 
+		first_touch t1 
+	join 
+		last_touch t2 
+		on t1.customer_id = t2.customer_id 
+) 
+, month_tenure as ( 
+-- As we assume that subscriptions always starts from the 1st day of the month and end on the last day of the month, with renewals occuring on the 1st day of the following month, we need to change the 'month_tenure' value from 0 to 1 for reccuring accounts and subscribers who canceled in the month immediately following their subscription. 
+-- e.g., reccuring subscriber '116156335' signing-up August 14, 2023 and cancelling on September 8, 2023 
+-- e.g., churned  subscriber '123169152' signing-up December 10, 2022 and cancelling on January 1, 2023 
+	select 
+		* 
+		, case 
+			when month_tenure = 0 and to_char(first_touch_date, 'YYYY-MM') != to_char(last_touch_date, 'YYYY-MM') then 1
+			else month_tenure 
+			end 
+		as real_month_tenure 
+	from 
+		subscribers_tenure
+) 
+select 
+	distinct *
+	, sum(accounts) over(order by month_tenure_bis rows between unbounded preceding and current row) as cumulative_accounts
+	, round(100.0*sum(accounts) over(order by month_tenure_bis rows between unbounded preceding and current row) / sum(accounts) over(), 1) as prop_cumulative_accounts
+from (
+	select 
+		real_month_tenure
+		, count(distinct customer_id) as accounts
+	from 
+		month_tenure 
+	group by 
+		1  
+) a
